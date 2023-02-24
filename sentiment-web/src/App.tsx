8746f7c8af8788a46fdbba0b9b46f595173933fa
Faisal Kabir Galib <faisal@dana.money>
2023-02-24 17:22:14 +0600

    add css init

---
+++ sentiment-web/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      {/* section for sentiment analysis */}
+      <section className='min-h-100vh flex'>
+        <p className='font-bold'>Sentiment Analysis</p>
+
+        <div className="flex flex-col">
+
+          <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+          <textarea id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+
+
+
+
+        </div>
+
+
+      </section>
+
+
+
+
+    </div>
+  )
+}
+
+export default App
